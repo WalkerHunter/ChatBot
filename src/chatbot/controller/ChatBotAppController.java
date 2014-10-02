b88c5fedc938244c247ed31a2b2b01a8e807a851
WalkerHunter <walkerhunter222@gmail.com>
2014-10-02 12:11:43 -0600

    Cleaned the quit model

--- src/chatbot/controller/ChatBotAppController.java
+++ src/chatbot/controller/ChatBotAppController.java
@@ -5,24 +5,41 @@ import javax.swing.JOptionPane;
 import chatbot.model.ChatBot;
 import chatbot.view.ChatBotView;
 
+/**
+ * Runs the ChatBot Project. Owns the model and associated views.
+ * @author Walker Hunter
+ * @version 1.2 10/2/14 - cleaned the quit method.
+ */
 public class ChatBotAppController
 {
 	private ChatBotView applicationView;
 	private ChatBot mySillyChatBot;
+	private String startMessage;
+	private String quitMessage;
+	
+	
 	
 	public ChatBotAppController()
 	{
 		applicationView = new ChatBotView(this);
 		mySillyChatBot = new ChatBot("Derf");
+		startMessage = "Welcome to the " + mySillyChatBot.getName() + " chatbot. What is your name?";
+		quitMessage = "goodbye cruel user :(";
 	}
-
+	
+	public ChatBot getMySillyChatBot()
+	{
+		return mySillyChatBot;
+	}
+	
+	
 	public void start()
 	{
-		String result = applicationView.showChatBot("Walker");
+		String result = applicationView.showChatBotDialog(startMessage);
 
 		while(!mySillyChatBot.quitChecker(result))
 		{
-			result = applicationView.showChatBot(result);
+			result = applicationView.showChatBotDialog(result);
 		}
 		
 		quit();
@@ -30,7 +47,7 @@ public class ChatBotAppController
 
 	private void quit()
 	{
-		JOptionPane.showMessageDialog(null, "good bye cruel world");
+		applicationView.showChatBotMessage(quitMessage);
 		System.exit(0);
 	}
 
