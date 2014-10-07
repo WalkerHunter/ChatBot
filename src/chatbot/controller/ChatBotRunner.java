6d096ab8ec4f295d59abbc94e1f817416453571d
WalkerHunter <walkerhunter222@gmail.com>
2014-10-07 13:37:14 -0600

    Documentation

--- src/chatbot/controller/ChatBotRunner.java
+++ src/chatbot/controller/ChatBotRunner.java
@@ -1,7 +1,18 @@
 package chatbot.controller;
 
+/**
+ * The runner for ChatBot.
+ * @author Walker Hunter
+ * @version 10/1/14
+ */
+
+
 public class ChatBotRunner
 {
+	/**
+	 * Starting point for all Java programs.
+	 * @param args Unused!
+	 */
 	public static void main(String[] args)
 	{
 		ChatBotAppController appController = new ChatBotAppController();
