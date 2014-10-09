b5540d16b4c008139a2e01c237e0ef54ce67dad1
WalkerHunter <walkerhunter222@gmail.com>
2014-10-09 12:50:12 -0600

    Added Checkers and Documentations

--- src/chatbot/model/ChatBot.java
+++ src/chatbot/model/ChatBot.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 /**
  * the ChatBot model class. Used for checking and manipulating strings.
- * @author Cody Henrichsen
- * @version 1.1 9/26/14
+ * @author Walker Hunter
+ * @version 1.6 10/9/14
  */
 public class ChatBot
 {
@@ -25,16 +25,29 @@ public class ChatBot
 	 * Sets the name of the ChatBot to the supplied name.
 	 * @param name The new name for the ChatBot.
 	 */
+	
+	/**
+	 * Gets the name of the person
+	 * @return The name of the ChatBot object
+	 */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * Counts the amount of times you've chat.
+	 * @return The number of chats.
+	 */
 	public int getChatCount()
 	{
 		return chatCount;
 	}
 
+	/**
+	 * Sets the name of the ChatBot to the supplied name
+	 * @param name Sets ChatBot's name.
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
@@ -59,28 +72,56 @@ public class ChatBot
 	public String processText(String currentInput)
 	{
 		String result = "";
-		if(memeChecker(currentInput))
+		
+		int randomPosition = (int) (Math.random() * 3);
+		
+		if(randomPosition == 0)
 		{
-			result = "wow, " + currentInput + " is a meme. Wahoo!";
+			if(stringChecker(currentInput))
+			{
+				result = "wow too much, take it easy";
+			}
+		}
+		else if(randomPosition == 1)
+		{
+			if(contentChecker(currentInput))
+			{
+				result = "Love that show!";
+			}
+			else
+			{
+				result = "meh, okay.";
+			}
 		}
 		else
 		{
-			result = "not a meme, try again";
+			if(memeChecker(currentInput))
+			{
+				result = "wow, " + currentInput + " is a meme. Wahoo!";
+			}
+			else
+			{
+				result = "not a meme, try again";
+			}
 		}
 		
-		if(stringChecker(currentInput))
-		{
-			result = "wow too much, take it easy";
-		}
+		
 		return result;
 	}
 
+	/**
+	 * Private helper method to update the chat count variable.
+	 */
 	private void updateChatCount()
 	{
 		chatCount++;
 	}
 
-	
+	/**
+	 * Private helper method to check meme's
+	 * @param input Meme's
+	 * @return If it is a meme or not
+	 */
 	private boolean memeChecker(String input)
 	{
 		boolean isAMeme = false;
@@ -104,7 +145,11 @@ public class ChatBot
 		return isAMeme;
 	}
 	
-	
+	/**
+	 * Public helper method to check to see if you want to quit
+	 * @param input Quitting the ChatBot
+	 * @return Says if you want to quit or not
+	 */
 	public boolean quitChecker(String input)
 	{
 		boolean okToQuit = false;
@@ -117,15 +162,36 @@ public class ChatBot
 		return okToQuit;
 	}
 
+	/**
+	 * Private helper method to check the length of the strings
+	 * @param input The amount of the string length
+	 * @return Will tell you if the string is too long
+	 */
 	private boolean stringChecker(String input)
 	{
-		boolean stringTooLong = false;
+		boolean isTooLong = false;
 		
-		if(input.length() > 20)
+		if(input.length() >= 20)
 		{
-			stringTooLong = true;
+			isTooLong = true;
 		}
-		return stringTooLong;
+		return isTooLong;
+	}
+	
+	/**
+	 * private helper method to check the content
+	 * @param input If they talk about supernatural
+	 * @return It will talk about supernatural as well
+	 */
+	private boolean contentChecker(String input)
+	{
+		boolean supernatural = false;
+		
+		if(input.contains("supernatural"))
+		{
+			supernatural = true;
+		}
+		return supernatural;
 	}
 	
 }
